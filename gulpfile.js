c63383c9b23fb321abd749134f82cecc6dbdb099
gucas <gucasphoto@mac.com>
2016-01-23 21:35:31 +0100

    Adjust

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+// Requis
+var gulp = require('gulp');
+
+// Include plugins
+var plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
+
+// VARIABLE
+var sourcePath         = './Projet/src/';
+var sourcePathCSS      =  sourcePath + './css/less/';
+
+var destinationPath    = './Projet/build/';
+var destinationPathCSS = destinationPath + './css/';
+var destinationPathJS  = destinationPath + './JS/';
+
+
+// STYLES
+// gulp.task('buildCSS', function () {
+//   return gulp.src(sourcePathCSS + '*.less')
+//     .pipe(plugins.less())
+//     .pipe(plugins.csscomb())
+//     .pipe(plugins.cssbeautify({indent: '  '}))
+//     .pipe(plugins.autoprefixer())
+//     .pipe(plugins.csso())
+//     .pipe(plugins.rename({
+//       suffix: '.min'
+//     }))
+//     .pipe(gulp.dest(destinationPathCSS));
+// });
+
+gulp.task('css', function () {
+    var postcss = require('gulp-postcss');
+    return gulp.src(sourcePathCSS + '*.less')
+        .pipe( postcss([ require('autoprefixer') )
+        .pipe( gulp.dest(destinationPathCSS) );
+});
+
+// Tâche par défaut
+gulp.task('default', ['buildCSS']);
